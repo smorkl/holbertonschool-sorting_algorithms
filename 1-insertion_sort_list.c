d707d9284ce94541102a9ba7eab8d7752afb0232
smorkl <smsalazar0319@gmail.com>
2024-04-29 20:06:43 -0500

    modificar

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,20 +2,33 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *head;
-    
-    node = node->next;
-    
-    while (node != NULL)
-    {
-        if (node->n < node->prev->n)
-        {
-            node = node->next;  
-        }
-        else
-        {
-            swap_nodes(node->prev);
-        }
-        node = node->next;     
-    }
+	listint_t *a_node, *b_node;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	b_node = (*list)->next;
+
+	while (b_node)
+	{
+		a_node = (*b_node).next;
+		while ((*b_node).prev && (*b_node).n < (*b_node).prev->n)
+		{
+			(*b_node).prev->next = (*b_node).next;
+			if ((*b_node).next)
+				(*b_node).next->prev = (*b_node).prev;
+			(*b_node).next = (*b_node).prev;
+			if ((*b_node).next)
+			{
+				(*b_node).prev = (*b_node).next->prev;
+				(*b_node).next->prev = b_node;
+			}
+			if ((*b_node).prev)
+				(*b_node).prev->next = b_node;
+			else
+				(*list) = b_node;
+			print_list(*list);
+		}
+		b_node = a_node;
+	}
 }
